2bb7323023140654c9937132002a6830d5bb674c
pookey1104 <pookey1104@naver.com>
2024-03-06 17:38:10 +0900

    css 추가 수정2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,57 +7,48 @@ import InstaImg from "../img/instagram.png";
 import Cursor from "../img/home_cursor.png";
 
 function Home() {
-    return (
-        <div className="page">
-            <div className="page-bg">
-                <div className="page-space">
-                    <Header_Home />
-                    <div className="center">
-                        <div className="content">
-                            <div className="home-com1">
-                                <Link to="/about" className="homeToAbout">
-                                    <div>LIKELION</div>
-                                    <div>UNIV</div>
-                                    <div>12TH</div>
-                                    <img
-                                        src={Cursor}
-                                        className="CursorImg"
-                                    ></img>
-                                </Link>
-                                <div className="grayBox"></div>
-                            </div>
-                            <div className="home-com2">
-                                <Link to="/input" className="homeToInput">
-                                    <div>합격자</div>
-                                    <div>조회</div>
-                                </Link>
-                            </div>
-                            <div className="home-com3">
-                                <Link to="/input" className="pointBox">
-                                    <img src={Point} className="PointImg"></img>
-                                </Link>
-                                <Link
-                                    to="https://www.instagram.com/likelion_ds?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
-                                    className="homeToInsta"
-                                >
-                                    <img
-                                        src={InstaImg}
-                                        className="InstaImg"
-                                    ></img>
-                                </Link>
-                                <div className="blackBox">
-                                    <div>GROWL</div>
-                                    <div>TO</div>
-                                    <div>WORLD !</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <Footer />
+  return (
+    <div className="page">
+      <div className="page-bg">
+        <div className="page-space">
+          <Header_Home />
+          <div className="center">
+            <div className="home-content">
+              <div className="home-com1">
+                <Link to="/about" className="homeToAbout">
+                  <div>LIKELION</div>
+                  <div>UNIV</div>
+                  <div>12TH</div>
+                  <img src={Cursor} className="CursorImg"></img>
+                </Link>
+                <div className="grayBox"></div>
+              </div>
+              <div className="home-com2">
+                <Link to="/input" className="homeToInput">
+                  <div>합격자</div>
+                  <div>조회</div>
+                </Link>
+              </div>
+              <div className="home-com3">
+                <Link to="/input" className="pointBox">
+                  <img src={Point} className="PointImg"></img>
+                </Link>
+                <Link to="https://www.instagram.com/likelion_ds?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" className="homeToInsta">
+                  <img src={InstaImg} className="InstaImg"></img>
+                </Link>
+                <div className="blackBox">
+                  <div>GROWL</div>
+                  <div>TO</div>
+                  <div>WORLD !</div>
                 </div>
+              </div>
             </div>
+          </div>
+          <Footer />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Home;
