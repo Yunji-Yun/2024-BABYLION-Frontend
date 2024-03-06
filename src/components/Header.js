e3f9c72fb051a94e94482b8f8de1382e6f3061cd
pookey1104 <pookey1104@naver.com>
2024-03-06 17:09:53 +0900

    css 추가 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import "../styles/Header.css";
+import back from "../img/back.png";
 
 function Header() {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
   return (
-    <div className="Header">
-      <p>LIKELION.</p>
-      <p className="Header-ds">DS</p>
+    <div className="Header-header">
+      <div className="Header-back">
+        <img src={back} alt="뒤로가기" onClick={handleBack} />
+      </div>
+      <div className="Header">
+        <p>LIKELION.</p>
+        <p className="Header-ds">DS</p>
+      </div>
     </div>
   );
 }
--- src/components/Header_About.js
+++ src/components/Header_About.js
@@ -1,9 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import "../styles/Header.css";
+import back from "../img/back.png";
 
 function Header_About() {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div className="Header-center">
       <div className="Header_About">
+        <div className="Header-back">
+          <img src={back} alt="뒤로가기" onClick={handleBack} />
+        </div>
         <p>GROWL -</p>
         <div className="Header-about-low">
           <p>TO</p>
